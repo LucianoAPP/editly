8267823169269338b2caa06b543cb972da2ea449
lap <flavioallcca@gmail.com>
2023-12-03 18:33:27 -0500

    add ,'interactive-text'

--- parseConfig.js
+++ parseConfig.js
@@ -91,7 +91,7 @@ export default async function parseConfig({ defaults: defaultsIn = {}, clips, ar
       return outLayers;
     }
 
-    if (['title', 'subtitle', 'news-title', 'slide-in-text'].includes(type)) {
+    if (['title', 'subtitle', 'news-title', 'slide-in-text', 'interactive-text'].includes(type)) {
       assert(layer.text, 'Please specify a text');
 
       let { fontFamily } = layer;
